c5c4e1a3a3697bec2e7024c012b874d66ebefb9b
caxar <ololohkalool@gmail.com>
2024-04-16 20:53:32 +0300

    анимация сайта исправление багов

--- js/main.js
+++ js/main.js
@@ -18,16 +18,129 @@ $(function () {
   $(window).scroll(function () {
     if ($(this).scrollTop() > 0 && $(window).width() > 768) {
       header.addClass("sticky");
-      $("body").css({
-        paddingTop: hederHeight + "px", // делаем отступ у body, равный высоте шапки
-      });
+      // $("body").css({
+      //   paddingTop: hederHeight + "px", // делаем отступ у body, равный высоте шапки
+      // });
     } else {
       header.removeClass("sticky");
-      $("body").css({
-        paddingTop: 0, // удаляем отступ у body, равный высоте шапки
-      });
+      // $("body").css({
+      //   paddingTop: 0, // удаляем отступ у body, равный высоте шапки
+      // });
     }
   });
 });
 
-// Parallax
+// Удалить ссылку якоря при обновлении страницы из URL
+
+history.pushState("", document.title, window.location.pathname);
+
+// Прокрутка по якорям с добавлением отступа
+
+$(document).ready(function () {
+  $(".nav-menu li a").click(function () {
+    /*задали какой мы хотим отступ от верха страницы*/
+    var otstupTop = 60;
+    $("body,html").animate(
+      {
+        /*получили положение элемента вычли отступ и прокрутили*/
+        scrollTop: $($(this).attr("href")).offset().top - otstupTop,
+      },
+      100
+    );
+  });
+});
+
+// маска паттерн для ввода номера
+window.addEventListener("DOMContentLoaded", function () {
+  [].forEach.call(document.querySelectorAll(".tel"), function (input) {
+    var keyCode;
+    function mask(event) {
+      event.keyCode && (keyCode = event.keyCode);
+      var pos = this.selectionStart;
+      if (pos < 3) event.preventDefault();
+      var matrix = "+7 (___) ___ ____",
+        i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = this.value.replace(/\D/g, ""),
+        new_value = matrix.replace(/[_\d]/g, function (a) {
+          return i < val.length ? val.charAt(i++) : a;
+        });
+      i = new_value.indexOf("_");
+      if (i != -1) {
+        i < 5 && (i = 3);
+        new_value = new_value.slice(0, i);
+      }
+      var reg = matrix
+        .substr(0, this.value.length)
+        .replace(/_+/g, function (a) {
+          return "\\d{1," + a.length + "}";
+        })
+        .replace(/[+()]/g, "\\$&");
+      reg = new RegExp("^" + reg + "$");
+      if (
+        !reg.test(this.value) ||
+        this.value.length < 5 ||
+        (keyCode > 47 && keyCode < 58)
+      ) {
+        this.value = new_value;
+      }
+      if (event.type == "blur" && this.value.length < 5) {
+        this.value = "";
+      }
+    }
+
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false);
+  });
+});
+
+// Кнопка вверх
+
+const btnUp = {
+  el: document.querySelector(".btn-scroll"),
+  show() {
+    // удалим у кнопки класс btn-scroll_hide
+    this.el.classList.remove("btn-scroll_hide");
+  },
+  hide() {
+    // добавим к кнопке класс btn-scroll_hide
+    this.el.classList.add("btn-scroll_hide");
+  },
+  addEventListener() {
+    // при прокрутке содержимого страницы
+    window.addEventListener("scroll", () => {
+      // определяем величину прокрутки
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
+      scrollY > 400 ? this.show() : this.hide();
+    });
+    // при нажатии на кнопку .btn-scroll
+    document.querySelector(".btn-scroll").onclick = () => {
+      // переместим в начало страницы
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    };
+  },
+};
+
+btnUp.addEventListener();
+
+// Менять цвет кнопки наверх в области футер
+window.onscroll = function () {
+  var button = document.querySelector(".btn-scroll_content");
+  var footer = document.querySelector(".footer");
+
+  var buttonPosition = button.getBoundingClientRect();
+  var footerPosition = footer.getBoundingClientRect();
+
+  if (buttonPosition.bottom >= footerPosition.top) {
+    button.style.backgroundColor = "#2dca74";
+  } else {
+    button.style.backgroundColor = "#000";
+  }
+};
