e5a5b58a8b630f781492e87ca2ab3d344c55b39c
caxar <ololohkalool@gmail.com>
2024-04-16 19:38:24 +0300

    анимация блоков при скролле

--- js/main.js
+++ js/main.js
@@ -9,3 +9,25 @@ document.querySelectorAll(".accordion_tab").forEach((tab) => {
     tab.classList.add("active");
   });
 });
+
+// Скролл для шапки сайта и вычисление высоты
+
+$(function () {
+  let header = $("#header");
+  let hederHeight = header.height();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0 && $(window).width() > 768) {
+      header.addClass("sticky");
+      $("body").css({
+        paddingTop: hederHeight + "px", // делаем отступ у body, равный высоте шапки
+      });
+    } else {
+      header.removeClass("sticky");
+      $("body").css({
+        paddingTop: 0, // удаляем отступ у body, равный высоте шапки
+      });
+    }
+  });
+});
+
+// Parallax
